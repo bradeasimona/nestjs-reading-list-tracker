6a1bce49f0674776fe03d93ce72a2fcd73e462ba
bradeasimona <simona_bradea@flutterstudios.com>
2026-02-03 15:37:19 +0200

    feat: added API versioning (#2)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableVersioning();
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Reading List Tracker API')
     .setDescription('API for tracking reading lists')
@@ -12,7 +14,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
